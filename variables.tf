60ff93d8823482b803b02fc471f36e9e44d88be8
M Zain <159163782+Genda-phool@users.noreply.github.com>
2025-10-01 21:58:43 +0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "aws_region" { default = "us-east-1" }
-variable "my_home_ip" { description = "Your home public IP in CIDR (e.g. 1.2.3.4/32)" }
+variable "my_home_ip" { description = "home public IP in CIDR (e.g. 1.2.3.4/32)" }
 variable "db_password" { description = "RDS master password" }
 variable "app_name" { default = "demo-node-app" }
 variable "app_port" { default = 3000 }
